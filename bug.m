56c78960d327bc2ee5872ff6bf2dd6cbd4703369
BugHunter-X <askzenai@gmail.com>
2025-01-18 18:10:06 +0100

    Add bug.m file

---
+++ bug.m
@@ -0,0 +1,12 @@
+function result = myFunction(input)
+  % Some code here that might cause an error
+  if input < 0
+    error('Input must be non-negative');
+  end
+  result = someCalculation(input);
+end
+
+function output = someCalculation(x)
+  % This function might produce unexpected results for certain inputs
+  output = x / (x - 5);
+end
